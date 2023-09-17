45274dd607896d465d8fb35896ab99de7f1f5580
wen jen <wjen1214@gmail.com>
2023-09-17 11:07:44 -0700

    remove devtools in prod

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
